9f0396deb326558e96ee5e5e85243dbec0488df2
Sonu Kumar <sksonu8544@gmail.com>
2024-02-06 23:21:33 +0530

    fix image animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,10 +9,10 @@ const Header = () => {
   return (
     <header id="home">
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5>Hello i'm</h5>
         <h1 > Sonu Kumar </h1>
         <div className="header__content">
-          <h3 className="text-light">I A'm:  </h3>
+          <h3 className="text-light">I a'm:  </h3>
           <span>
             <TypeWriter  />
           </span>
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -15,42 +15,42 @@ const Testimonials = () => {
     {
       id: 1,
       link: 'https://www.linkedin.com/in/sonu8544/',
-      name: 'Uzair Ali',
+      name: 'Aditya Singh',
       role: 'Full-Stack Web Developer. JavaScript | Rails | React | Redux. Improving open-source projects, one commit at a time.',
       test: 'As a developer and a problem solver, I think Sonu is a great collaborative partner to have. I met Sonu in some basic javascript & react projects and since then she has drastically progressed in her understanding of the development process. She always has a professional environment and has good audio and video quality which makes it easier to communicate with her.',
     },
     {
       id: 2,
       link: 'https://www.linkedin.com/in/sonu8544/',
-      name: 'Yishak Wesego',
+      name: 'Kashyap Pavra',
       role: 'Full-stack developer | Technical Support Engineer at Microverse',
       test: "It was a pleasure collaborating with Sonu on different projects. One of the things that I think is special about her is that she never settles, even after completing the projects she finds other resources and strengthens her knowledge. Collaborating with her is easy and comfortable, it's like working with someone you've known for a long period of time.",
     },
     {
       id: 3,
       link: 'https://www.linkedin.com/in/sonu8544/',
-      name: 'akbar (Aku) Khan',
+      name: 'Rahul',
       role: 'Full Stack Developer| Ruby on Rails | PostgreSQL | JavaScript | React | Redux | Html&Css | Python.',
       test: 'I worked with Sonu in the same team and her communication skills are very strong. Her programming skill is one of the bests, given the time frame in the field. She is a good team player.  She will probably fit in most of the companies not only because she\'s a good team worker, but also because she has very good skills and I know she has much more potential to be shown.',
     },
     {
       id: 4,
       link: 'https://www.linkedin.com/in/sonu8544/',
-      name: 'Isai Céspedes',
+      name: 'Rushi Joshi',
       role: 'Full-Stack Web Developer. JavaScript | Rails | React | Redux.',
       test: "I mentored Sonu some months ago, and I can say that she is one of those people that you love to work with. She was always interested in what I was trying to teach her, paying attention and always asking questions if something was not clear. When it comes to professional skills, she is really committed to work, always doing her best and going beyond the requirements of the project she's building.",
     },
     {
       id: 5,
       link: 'https://www.linkedin.com/in/sonu8544/',
-      name: 'Hamza Tarar',
+      name: 'Prachi',
       role: 'Software Developer',
       test: "Throughout all our collaborations, Sonu has always conducted herself politely and kindly. She comes across as someone that's always willing to help and puts the team ahead of herself. But beneath this, I see a strength and determination to distinguish herself. She's not only someone I highly recommend but is also someone I greatly respect.",
     },
     {
       id: 6,
       link: 'https://www.linkedin.com/in/sonu8544/',
-      name: 'Htet (Rex) Naing',
+      name: 'Nitish',
       role: 'Full Stack Developer | Mindful Mentor | Cheerful Team Player',
       test: "Sonu is really cheerful and supportive person. I know her when she reviewed my resume. Her feedback was so crystal clear and super effective for me. After that, even though she has no responsibility of reviewing my resume again. She helped me patiently when I ask her again and again. Sonu is gifted in reviewing the work and giving advice to others. I feel really lucky to have her as the reviewer of my resume.",
     },
@@ -64,7 +64,7 @@ const Testimonials = () => {
         modules={[Pagination, Autoplay]}
         spaceBetween={40}
         slidesPerView={1}
-        breakpoints={{ 600: { slidesPerView: 2,}}}
+        breakpoints={{ 600: { slidesPerView: 2, } }}
         pagination={{ clickable: true }}
         autoplay={{ delay: 4000 }}
         loop={true}
