e5c0eabaff2c17d6cd40302d1c551fa4ab8cb411
Sonu Kumar <sksonu8544@gmail.com>
2024-03-13 23:53:48 +0530

    fix project link

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { FaAward } from 'react-icons/fa';
 import { VscFolderLibrary } from 'react-icons/vsc';
-// import ME from '../../assets/me.jpg';
 import ME from '../../assets/sonu.jpg';
 import './intro.css';
 
