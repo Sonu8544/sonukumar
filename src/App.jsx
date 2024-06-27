ce244a87c65286c444f91b0d2d68304648c8b62e
sonu kumar <sonu@beyoiwellness.com>
2024-06-27 14:23:35 +0530

    fix project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import Intro from './components/intro/Intro';
-import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
-import Topbar from './components/topbar/Topbar'; 
+import React from "react";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Testimonials from "./components/testimonials/Testimonials";
+import Topbar from "./components/topbar/Topbar";
 
 const App = () => {
   return (
@@ -16,11 +16,11 @@ const App = () => {
       <Intro />
       {/* <Experience /> */}
       <Portfolio />
-      {/* <Testimonials /> */}
+      <Testimonials />
       {/* <Contact /> */}
       {/* <Footer /> */}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
